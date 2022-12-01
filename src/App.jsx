1056baf73d8f03fd99e9236454d89a05dad7f2f1
mineralaz <jakimaviciuspovilas@gmail.com>
2022-12-01 09:43:46 +0200

    exported styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import styled from 'styled-components';
 import './App.css';
-
-// sukuriam h1 elementa pavadinimu Title naudodami styled components
-const Title = styled.h1`
-  font-size: 3rem;
-  font-weight: normal;
-  text-decoration: underline;
-`;
-
-// sukurti button komponenta, prideti jam stiliaus
-const Button = styled.button`
-  background-color: brown;
-  padding: 0.5em 1em;
-
-  font-size: 2rem;
-  color: white;
-  transition: all 0.3s;
-  &:hover {
-    background-color: green;
-    padding: 1em 2em;
-  }
-`;
-// panaudoti jsx
+import { Title, Button, LargeButton } from './components/styled/Demo.styled.js';
 
 function App() {
   return (
     <div className="App">
       <Title>React styled components</Title>
       <Button>Styled button component</Button>
+      <Button sm>Styled button component</Button>
+      <LargeButton sm>Styled button component</LargeButton>
     </div>
   );
 }
---
+++ src/components/styled/Demo.styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+// sukuriam h1 elementa pavadinimu Title naudodami styled components
+export const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: normal;
+  text-decoration: underline;
+`;
+
+// sukurti button komponenta, prideti jam stiliaus
+export const Button = styled.button`
+  background-color: brown;
+  padding: 0.5em 1em;
+  font-size: ${(props) => (props.sm ? '0.9rem' : '1.5rem')};
+  color: white;
+  transition: all 0.3s;
+  &:hover {
+    background-color: white;
+    color: brown;
+  }
+`;
+// panaudoti jsx
+// large button, paveldi viska is button, perrasom ka mums reikia
+export const LargeButton = styled(Button)`
+  padding: 0.9em 1.8em;
+  text-transform: uppercase;
+`;
