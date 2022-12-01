69f79e2d519279a1429a59fbd91d85b5066c5b1f
mineralaz <jakimaviciuspovilas@gmail.com>
2022-12-01 09:23:35 +0200

    button

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,26 @@ const Title = styled.h1`
   text-decoration: underline;
 `;
 
+// sukurti button komponenta, prideti jam stiliaus
+const Button = styled.button`
+  background-color: brown;
+  padding: 0.5em 1em;
+
+  font-size: 2rem;
+  color: white;
+  transition: all 0.3s;
+  &:hover {
+    background-color: green;
+    padding: 1em 2em;
+  }
+`;
+// panaudoti jsx
+
 function App() {
   return (
     <div className="App">
       <Title>React styled components</Title>
+      <Button>Styled button component</Button>
     </div>
   );
 }
